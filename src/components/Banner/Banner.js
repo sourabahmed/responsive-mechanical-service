d02eedb8c492a4da7a8614bb62e27d60f0eb1640
unknown <sourabahmed>
2021-10-12 23:34:27 +0600

    banner-added

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
+import banner1 from '../../Images/banner/banner-1.jpg'
+import banner2 from '../../Images/banner/banner-2.jpg'
+import banner3 from '../../Images/banner/banner-3.jpg'
+import './Banner.css'
 
 const Banner = () => {
     return (
-        <div>
+        <div className="banner">
             <Carousel>
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src="holder.js/800x400?text=First slide&bg=373940"
+                        src="http://www.simpleimageresizer.com/_uploads/photos/c925945a/istockphoto-914314364-170667a_1300x500.jpg"
                         alt="First slide"
                     />
                     <Carousel.Caption>
@@ -19,7 +23,7 @@ const Banner = () => {
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src="holder.js/800x400?text=Second slide&bg=282c34"
+                        src="http://www.simpleimageresizer.com/_uploads/photos/c925945a/istockphoto-1285071153-170667a_1300x500.jpg"
                         alt="Second slide"
                     />
 
@@ -31,7 +35,7 @@ const Banner = () => {
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src="holder.js/800x400?text=Third slide&bg=20232a"
+                        src="http://www.simpleimageresizer.com/_uploads/photos/c925945a/istockphoto-1289143731-170667a_1300x500.jpg"
                         alt="Third slide"
                     />
 
